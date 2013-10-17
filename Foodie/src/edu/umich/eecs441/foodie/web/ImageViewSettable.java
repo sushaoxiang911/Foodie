01716e8cfbbcc5676f8ace9a171a2ec86f452285
sushaoxiang911 <ssx@umich.edu>
2013-10-17 17:02:16 -0400

    Damn

---
+++ Foodie/src/edu/umich/eecs441/foodie/web/ImageViewSettable.java
@@ -0,0 +1,7 @@
+package edu.umich.eecs441.foodie.web;
+
+import android.graphics.Bitmap;
+
+public interface ImageViewSettable extends ProgressDialogApplicable {
+	public void setImageView(Bitmap bm);
+}
