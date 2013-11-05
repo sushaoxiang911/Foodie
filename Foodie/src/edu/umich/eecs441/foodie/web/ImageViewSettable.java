1f4a136d9e800e3cf64e63c8ba8ceb5948366237
sushaoxiang911 <ssx@umich.edu>
2013-11-05 17:39:50 -0500

    Receive Translation

--- Foodie/src/edu/umich/eecs441/foodie/web/ImageViewSettable.java
+++
@@ -1,7 +0,0 @@
-package edu.umich.eecs441.foodie.web;
-
-import android.graphics.Bitmap;
-
-public interface ImageViewSettable extends ProgressDialogApplicable {
-	public void setImageView(Bitmap bm);
-}
