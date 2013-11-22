8e77ba04c40caa0f193f610463ef35f1335d232b
Bingjie Xu <bingjiex@umich.edu>
2013-11-22 00:35:28 -0500

    1)Result UI & Preview UI finished (may have bugs)2) Logo updated

--- Foodie/gen/com/example/foodie/R.java
+++ Foodie/gen/com/example/foodie/R.java
@@ -26,22 +26,25 @@ public final class R {
         public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080005;
-        public static final int action_settings=0x7f08000b;
-        public static final int box_preview=0x7f080001;
-        public static final int button1=0x7f080007;
-        public static final int button2=0x7f080008;
-        public static final int button_capture=0x7f080002;
-        public static final int button_new=0x7f080003;
-        public static final int camera_preview=0x7f080000;
-        public static final int imageView1=0x7f080009;
-        public static final int imageView2=0x7f080006;
-        public static final int result_preview=0x7f080004;
-        public static final int textView2=0x7f08000a;
+        public static final int LinearLayout1=0x7f080008;
+        public static final int action_settings=0x7f08000d;
+        public static final int box_preview=0x7f080004;
+        public static final int button1=0x7f080001;
+        public static final int button2=0x7f08000a;
+        public static final int button_capture=0x7f080005;
+        public static final int button_new=0x7f080006;
+        public static final int camera_preview=0x7f080003;
+        public static final int editText1=0x7f080000;
+        public static final int imageView1=0x7f08000b;
+        public static final int imageView2=0x7f080009;
+        public static final int result_preview=0x7f080007;
+        public static final int textView1=0x7f080002;
+        public static final int textView2=0x7f08000c;
     }
     public static final class layout {
-        public static final int camera_activity=0x7f030000;
-        public static final int recognize_activity=0x7f030001;
+        public static final int activity_web_test=0x7f030000;
+        public static final int camera_activity=0x7f030001;
+        public static final int recognize_activity=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
