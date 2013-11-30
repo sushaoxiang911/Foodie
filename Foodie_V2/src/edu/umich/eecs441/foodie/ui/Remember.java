ad7a8d7cc1e29cae8351620e49af71c2dc4dbabd
sushaoxiang911 <ssx@umich.edu>
2013-11-30 17:14:32 -0500

    add new classes

---
+++ Foodie_V2/src/edu/umich/eecs441/foodie/ui/Remember.java
@@ -0,0 +1,9 @@
+package edu.umich.eecs441.foodie.ui;
+
+public class Remember {
+	// remember name and password
+	public static final String PREFS_NAME = "PrefsFile";
+	public static final String PREF_USERNAME = "username";
+	public static final String PREF_PASSWORD = "password";
+	public static final String IF_PREF = "ifpref";
+}
